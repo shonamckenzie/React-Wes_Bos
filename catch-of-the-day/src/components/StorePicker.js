327196a655d1c6f903ae51065d4b10e89eadf1b6
Shona McKenzie <shona@traveltek.net>
2021-12-22 16:47:48 +0000

    adds storepicker component and mount the app

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// each component will be a class, each class needs at least 1 method - render determines what elements to put on the page
+class StorePicker extends React.Component {
+	render() {
+		return <p>I am the store Picker!</p>
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'
+
+import StorePicker from './components/StorePicker'
+
+// render method takes the React component to be rendered & the DOM element it should be appended to, id main is defined in index.html.  aka mounting the application
+// render method takes 2 parameters - JSX and the mounting point
+render(<StorePicker/>, document.querySelector('#main'));
