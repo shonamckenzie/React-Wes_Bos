e709f1625c1857aef66b61d4eed17854d452c24b
Shona McKenzie <shona@traveltek.net>
2022-01-04 16:43:00 +0000

    adds fishform and sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,43 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+	// method that updates state and the state need to exist in same component
+	state = {
+		fishes: {},
+		order: {}
+	};
+	addFish = (fish) => {
+		// take copy of existing state
+		const fishes = {...this.state.fishes};
+		// add new fish to fishes variable
+		fishes[`fish${Date.now()}`] = fish;
+		// set new fishes object to state
+		this.setState({
+			fishes: fishes
+		})
+	}
+	loadSampleFishes = () => {
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					{/* return Header component and pass tagline as prop */}
 					<Header tagline='Fresh Seafood Market'></Header>
+					<ul className="fishes">
+						{Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}></Fish>)}
+					</ul>
 				</div>
 				<Order></Order>
-				<Inventory></Inventory>
+				{/* pass addFish method as props to Inventory component */}
+				<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}></Inventory>
 			</div>
 		)
 	}
--- catch-of-the-day/src/helpers.js
+++ catch-of-the-day/src/helpers.js
@@ -1,7 +1,7 @@
-export function formatPrice(cents) {
-  return (cents / 100).toLocaleString("en-US", {
+export function formatPrice(pence) {
+  return (pence / 100).toLocaleString("en-UK", {
     style: "currency",
-    currency: "USD"
+    currency: "GBP"
   });
 }
 
