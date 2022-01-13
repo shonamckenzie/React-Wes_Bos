ed16fd60b8828c9c938f6c9f4f70a87251107da7
Shona McKenzie <shona@traveltek.net>
2022-01-13 14:27:12 +0000

    set up animations

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { formatPrice } from '../helpers'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Order extends React.Component {
 	renderOrder = key => {
@@ -8,18 +9,35 @@ class Order extends React.Component {
 		
 		// as we're now setting state with localStorage and then doing syncState we need to wait for the DB to return the fishes
 		const isAvailable = fish && fish.status === 'available'
+		const transitionOptions = { 
+			classNames: "order",
+			key,
+			timeout: { enter: 500, exit:500}
+		};
+		
 		if (!fish) return null;
 		
 		if(!isAvailable) {
 			return (
-				<li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
+				<CSSTransition { ...transitionOptions }>
+					<li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
+				</CSSTransition>
 			)
 		}
 		return (
-			<li key={key}>
-				{count} lbs {fish.name} {formatPrice(count * fish.price)} 
-				<button onClick={() => this.props.removeFromOrder(key)}>&times;</button> 
-			</li>
+			<CSSTransition { ...transitionOptions }>
+				<li key={key}>
+					<span>
+						<TransitionGroup component="span" className="count">
+							<CSSTransition classNames="count" key={count}>
+								<span>{count}</span> 
+							</CSSTransition>
+						</TransitionGroup>
+						lbs {fish.name} {formatPrice(count * fish.price)} 
+						<button onClick={() => this.props.removeFromOrder(key)}>&times;</button> 
+					</span>
+				</li>
+			</CSSTransition>
 		)
 	};
 	render() {
@@ -36,9 +54,9 @@ class Order extends React.Component {
 		return (
 		<div className="order-wrap">
 			<h2>Order</h2>
-			<ul className="order">
+			<TransitionGroup component="ul" className="order">
 			{orderIds.map(this.renderOrder)}
-			</ul>
+			</TransitionGroup>
 			<div className="total">
 				Total:
 				<strong>{formatPrice(total)}</strong>
